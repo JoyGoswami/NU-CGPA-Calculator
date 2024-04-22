1934a4fbd9532e0cfd999090847123bc65965bbb
Joy Goswami <joy63586@gmail.com>
2024-04-22 13:51:44 +0600

    second year added

--- script2.js
+++ script2.js
@@ -53,6 +53,8 @@ const grades = [
 ];
 let creditArr = [];
 let gradeArr = [];
+let creditArr2 = [];
+let gradeArr2 = [];
 
 // Courses
 const subjectOptions = ["Select Course", "English", "Bangla"];
@@ -76,11 +78,11 @@ infoForm.addEventListener("submit", (e) => {
   const courseYear = document.getElementById("course_year").value;
 
   //display year
-  document.querySelector(".year").textContent =
-    courseData[courseName][courseYear].year;
+  let currentYear = courseData[courseName][courseYear].year;
+  document.querySelector(".year-1").textContent = currentYear;
 
   // selecting form body where child elements will be appened
-  const tableBody = document.getElementById("table_body");
+  const tableBody = document.getElementById("table_body-1");
 
   //create elements a tr and 4 tds
   const tr = document.createElement("tr");
@@ -126,18 +128,28 @@ infoForm.addEventListener("submit", (e) => {
   });
   // remove nu-calculate-home
   document.querySelector(".nu-calculate-home").style.display = "none";
-  document.querySelector(".nu-display-gpa").style.display = "block";
+  document.querySelector(".nu-display-gpa-1").style.display = "block";
+
+  //display another year options based on selected year. disable the selected year
+  const selectAnotherEl = document.getElementById("another-year");
+  selectAnotherEl.style.display = "block";
+  for (let i = 0; i < selectAnotherEl.options.length; i++) {
+    if (selectAnotherEl.options[i].label === currentYear) {
+      selectAnotherEl.options[i].disabled = true;
+    }
+  }
 });
 // ================================
 // ========page 2form submit========
 // ================================
 
 // ================================
-// ========page 3 form submit========
+// ========page 3 form 1 submit========
 // ================================
-const tableForm = document.getElementById("table-form");
+const tableForm = document.getElementById("table-form-1");
 tableForm.addEventListener("submit", function (e) {
   e.preventDefault();
+  document.querySelector(".result-1").style.display = "block";
 
   const selectElements = document.querySelectorAll(".select");
   gradeArr = [];
@@ -154,16 +166,111 @@ tableForm.addEventListener("submit", function (e) {
   });
   //   console.log(creditArr);
   //   console.log(gradeArr);
-  displayResult(creditArr, gradeArr);
+  const totalCreditEl1 = document.querySelector(".total-credit-1");
+  const gpaEl = document.querySelector(".gpa-1");
+  displayResult(creditArr, gradeArr, totalCreditEl1, gpaEl);
 });
 // ================================
-// ========page 3 form submit========
+// ========page 3 form 1 submit========
+// ================================
+
+// ================================
+// ========display another year on ui========
+// ================================
+const selectAnotherEl = document.getElementById("another-year");
+selectAnotherEl.addEventListener("change", function () {
+  const courseName = document.getElementById("course_name").value;
+  const courseYear = selectAnotherEl.value;
+  document.querySelector(".nu-display-gpa-2").style.display = "block";
+
+  //display year
+  let currentYear = courseData[courseName][courseYear].year;
+  document.querySelector(".year-2").textContent = currentYear;
+
+  // selecting form body where child elements will be appened
+  const tableBody = document.getElementById("table_body-2");
+
+  //create elements a tr and 4 tds
+  const tr = document.createElement("tr");
+  const tdSerialNo = document.createElement("td");
+  const tdSubjectName = document.createElement("td");
+  const tdCredit = document.createElement("td");
+  const tdGrade = document.createElement("td");
+
+  //append 4 tds to tr
+  tr.appendChild(tdSerialNo, tdSubjectName, tdCredit, tdGrade);
+
+  // getting the data from courseData and displaying it
+  courseData[courseName][courseYear].subjects.forEach((subject, index) => {
+    const tr = document.createElement("tr");
+    tableBody.appendChild(tr);
+    const tdSerialNo = document.createElement("td");
+    tdSerialNo.textContent = index + 1;
+    const tdSubjectName = document.createElement("td");
+    tdSubjectName.textContent = subject;
+    const tdCredit = document.createElement("td");
+    tdCredit.className = "credit";
+
+    // Storing credits into an array
+    // let creditArr = [];
+    let creditNum = Number(courseData[courseName][courseYear][subject]);
+    creditArr2.push(creditNum);
+    //
+    tdCredit.textContent = courseData[courseName][courseYear][subject];
+
+    const tdGrade = document.createElement("td");
+
+    const select = document.createElement("select");
+    select.name = "grade";
+    select.id = "grade";
+    select.className = "select2";
+
+    populateOptions(select, grades, "Select");
+
+    tdGrade.appendChild(select);
+
+    //append 4 tds to tr
+    tr.append(tdSerialNo, tdSubjectName, tdCredit, tdGrade);
+  });
+});
+// ================================
+// ========display another year on ui========
+// ================================
+
+// ================================
+// ========page 3 form 2 submit========
+// ================================
+const tableForm2 = document.getElementById("table-form-2");
+tableForm2.addEventListener("submit", function (e) {
+  e.preventDefault();
+  document.querySelector(".result-2").style.display = "block";
+
+  const selectElements = document.querySelectorAll(".select2");
+  gradeArr2 = [];
+  selectElements.forEach((select) => {
+    // store the grade values in gradeStr as string
+    let gradeStr = select.value;
+    // find the index of the first space to delete the first letter
+    let firstSpaceIndex = gradeStr.indexOf(" ");
+    let grades = gradeStr.slice(firstSpaceIndex + 1);
+    if (grades === "Select") {
+      grades = "0";
+    }
+    gradeArr2.push(Number(grades));
+  });
+
+  const totalCreditEl2 = document.querySelector(".total-credit-2");
+  const gpaEl2 = document.querySelector(".gpa-2");
+  displayResult(creditArr2, gradeArr2, totalCreditEl2, gpaEl2);
+});
+// ================================
+// ========page 3 form 2 submit========
 // ================================
 
 // ================================
 // ========functions========
 // ================================
-function displayResult(creditArr, gradeArr) {
+function displayResult(creditArr, gradeArr, totalCreditEl, gpaEl) {
   //calculate gpa
 
   // muliplying creditArr and gradeArr and storing into sgpa
@@ -184,9 +291,9 @@ function displayResult(creditArr, gradeArr) {
   let gpa = totalSgpa / totalCredit;
 
   //display values
-  document.querySelector(".total-credit").textContent = totalCredit;
-  document.querySelector(".gpa").textContent = gpa.toFixed(2);
-  const cgpaEl = document.querySelector(".cgpa");
+  totalCreditEl.textContent = totalCredit;
+  gpaEl.textContent = gpa.toFixed(2);
+  const cgpaEl = document.querySelector(".cgpa-1");
 }
 
 function populateOptions(parentEl, options, text) {
